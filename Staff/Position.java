fd8d0fd5d7118999ee5ba06c8d567b502a1a172f
Ryan Donnelly <99196770+MacedoEU@users.noreply.github.com>
2023-05-16 23:07:24 +0000

    commit

--- Staff/Position.java
+++ Staff/Position.java
@@ -2,4 +2,25 @@ package Staff;
 
 public enum Position {
     
+
+    FULL_TIME("Full Time", 100), 
+    PART_TIME("Part Time", 50); 
+
+    private final String role; 
+    private final double salary; 
+
+
+    Position(String role, double salary) {
+        this.role = role; 
+        this.salary = salary; 
+    }
+
+    public String getRole() {
+        return this.role; 
+    }
+
+    public double getSalary() {
+        return this.salary; 
+    }
+
 }
