14f8a6ade867902b925288846f022cdf4a8b35fc
Ryan Donnelly <99196770+MacedoEU@users.noreply.github.com>
2023-05-16 23:07:19 +0000

    Staff updated

--- Staff/Staff.java
+++ Staff/Staff.java
@@ -2,9 +2,28 @@ package Staff;
 
 public class Staff {
  
-    // default constructor 
-    public Staff () {
+    private String name; 
+    private Position role; 
 
+    public Staff (String name, Position role) {
+        this.name = name; 
+        this.role = role; 
+    }
+
+    protected String getName() {
+        return this.name; 
+    }
+
+    protected  Position getPosition() {
+        return this.role; 
+    }
+
+    protected void setName(String name) {
+        this.name = name;  
+    }
+
+    protected void setRole(Position role) {
+        this.role = role;
     }
 
 }
