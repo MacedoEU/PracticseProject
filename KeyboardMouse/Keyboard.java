167095da926d3774cf4c0e0c0eea2ae5669fe857
Ryan Donnelly <99196770+MacedoEU@users.noreply.github.com>
2023-05-16 19:36:02 +0000

    Completed Keyboard type

--- KeyboardMouse/Keyboard.java
+++ KeyboardMouse/Keyboard.java
@@ -2,18 +2,38 @@ package KeyboardMouse;
 
 public class Keyboard {
     
-    private int id; 
+    private int keyboardId; 
     private KeyboardType keyboardType;
     private double price;
     private Quality quality;
 
 
     // id - keyboard Type -- price -- QUALITY 
-    public Keyboard(int id, KeyboardType keyboardType, double price, Quality quality ) {
-        this.id = id; 
+    public Keyboard(int keyboardId, KeyboardType keyboardType, double price, Quality quality ) {
+        this.keyboardId = keyboardId; 
         this.keyboardType = keyboardType; 
         this.price = price; 
         this.quality = quality; 
+    }
+
+    protected void setPrice(double price) {
+        this.price = price;
+    }
+
+    protected int getID() {
+        return this.keyboardId; 
+    }
+
+    protected  KeyboardType getKeyboardType() {
+        return this.keyboardType; 
+    }
+
+    protected double getPrice() {
+        return this.price;
+    }
+
+    protected Quality geQuality() {
+        return this.quality; 
     }
 
     
