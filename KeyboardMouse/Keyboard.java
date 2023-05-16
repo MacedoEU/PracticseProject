027b9ade440ba322d6e4f761968b63580ac9fc2e
Ryan Donnelly <99196770+MacedoEU@users.noreply.github.com>
2023-05-16 17:48:46 +0000

    Updated keyboard object class

--- KeyboardMouse/Keyboard.java
+++ KeyboardMouse/Keyboard.java
@@ -2,6 +2,21 @@ package KeyboardMouse;
 
 public class Keyboard {
     
-    // id - manuafture - model -- quality  
+    private int id; 
+    private KeyboardType keyboardType;
+    private double price;
+    private Quality quality;
+
+
+    // id - keyboard Type -- price -- QUALITY 
+    public Keyboard(int id, KeyboardType keyboardType, double price, Quality quality ) {
+        this.id = id; 
+        this.keyboardType = keyboardType; 
+        this.price = price; 
+        this.quality = quality; 
+    }
+
+    
+
 
 }
