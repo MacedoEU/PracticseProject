5a373ea474bf943ebba2721e0268e8f81c18f80b
Ryan Donnelly <99196770+MacedoEU@users.noreply.github.com>
2023-05-19 15:43:41 +0000

    Updated changes on sjop staff

--- Shop/ShopAppStaff.java
+++ Shop/ShopAppStaff.java
@@ -2,4 +2,21 @@ package Shop;
 
 public class ShopAppStaff {
     
+    public static void main(String[] args) {
+        printMenu(); 
+    }
+
+    public static void printMenu() {
+        System.out.println(""); 
+        System.out.println("-------------------------------");  
+        System.out.println("1. Transaction a purchase"); 
+        System.out.println("2. Search for a product"); 
+        System.out.println("3  Reset a Password for a customer");
+        System.out.println("4. Display all Transactions");
+    }
+
+
+
+
+
 }
