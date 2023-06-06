4c56c7cb4529685cf1e118678ec3e05b4e8a70f3
Ryan Donnelly <99196770+MacedoEU@users.noreply.github.com>
2023-06-06 11:41:04 +0000

    Created login options for staff members

--- Shop/ShopAppStaff.java
+++ Shop/ShopAppStaff.java
@@ -1,5 +1,7 @@
 package Shop;
 
+import java.util.Scanner;
+
 public class ShopAppStaff {
     
     public static void main(String[] args) {
@@ -13,6 +15,54 @@ public class ShopAppStaff {
         System.out.println("2. Search for a product"); 
         System.out.println("3  Reset a Password for a customer");
         System.out.println("4. Display all Transactions");
+        System.out.println("5. Customer Menu");
+        System.out.println("-------------------------------");  
+        decision();
+    }
+
+    private static  void decision() {
+
+        try {
+            System.out.print("Input: "); 
+            Scanner sc = new Scanner(System.in); 
+            int choice = sc.nextInt(); 
+
+            if (choice == 1) {
+               
+            }
+
+            else if (choice == 2) {
+
+            }
+
+            else if (choice == 3 ) {
+
+            }
+
+            else if (choice == 4) {
+
+            }
+
+            else if (choice == 5) {
+
+            }
+
+            else {
+                System.out.println(""); 
+                System.out.println("An error has occured please try again"); 
+                System.out.println("");  
+                printMenu();
+            }
+
+            sc.close();
+        } 
+        catch (Exception e) {
+            System.out.println(""); 
+            System.out.println("An error has occured please try again"); 
+            System.out.println("");  
+            printMenu();
+        }
+
     }
 
 
